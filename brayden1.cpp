97326df17d1bd7cf64149e143cefe359e8180916
Brayden Thompson <brayden-g-t>
2026-04-01 23:00:07 -0700

    brayden1 commit

---
+++ brayden1.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file brayden1.cpp
+ * @author Brayden Thompson
+ * @brief my first "person" on lab8
+ * @version 62.0.1.0.2
+ * @date 2026-04-01
+ * 
+ * @copyright Thompson Supreme Code™
+ * 
+ */
+#include <iostream>
+
+ int main(){
+    std::cout << "hello i am brayden1!!\n";
+ }
